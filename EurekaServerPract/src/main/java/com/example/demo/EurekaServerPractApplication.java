908ba1fb0bd8808e34685725c79235b6dae6b5f7
SanketSahu1307 <you@example.com>
2020-02-09 14:21:45 +0530

    success

--- EurekaServerPract/src/main/java/com/example/demo/EurekaServerPractApplication.java
+++ EurekaServerPract/src/main/java/com/example/demo/EurekaServerPractApplication.java
@@ -2,14 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
-@EnableZuulServer
-public class ZuulapigatewayApplication {
+public class EurekaServerPractApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulapigatewayApplication.class, args);
+		SpringApplication.run(EurekaServerPractApplication.class, args);
 	}
 
 }
